590ad4824c1d0c877a2948493c50ccf070cc437a
brianalexandro <alexandrobrian15@gmail.com>
2016-01-04 12:18:25 +0700

    - privacy policy - contact us - about us - terms of use

---
+++ application/views/Elements/Subview/common/ads.php
@@ -0,0 +1,27 @@
+<div class="wrapper-ads">
+	<ul class="no-pd">
+		<?php
+				echo tag('img', false, array(
+					'src' => $domain.'/resources/images/sample-ads.jpg',
+					'wrapTag' => 'li',
+					'wrapAttributes' => array(
+						'class' => 'no-ul-type mb20',
+					),
+				));
+				echo tag('img', false, array(
+					'src' => $domain.'/resources/images/sample-ads.jpg',
+					'wrapTag' => 'li',
+					'wrapAttributes' => array(
+						'class' => 'no-ul-type mb20',
+					),
+				));
+				echo tag('img', false, array(
+					'src' => $domain.'/resources/images/sample-ads.jpg',
+					'wrapTag' => 'li',
+					'wrapAttributes' => array(
+						'class' => 'no-ul-type mb20',
+					),
+				));
+		?>
+	</ul>
+</div>
